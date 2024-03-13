bf4398766872acf9be7579d384a4a61eb40c0e97
GiftSibiya <giftsibiya.gb@gmail.com>
2024-03-13 08:55:07 +0200

    contact form layout

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,66 @@
+// Imports
 import React from "react";
 
 const Contact = () => {
-  return <div className="bg-gray-400 h-[200px]">Contact</div>;
+  return (
+    <div className="flex flex-col mt-[25px] items-center md:h-[450px]">
+      <div className="flex sm:flex-row flex-col w-[90%] justify-between ">
+        {/* Left Form Section */}
+        <div className="flex flex-col justify-center p-2 border-2 md:w-[700px] md:h-[350px]">
+          <h4 className="text-gray-300 text-2xl">Drop Me a message</h4>
+          <hr className="w-[300px] border-orange-500 border-2 rounded-lg " />
+          <p className="my-[10px] text-gray-300">
+            Get in touch with me for on mobile or web products you have in mind
+            and you will be one of my satisfied clients. <br />
+            <span className="text-orange-500">
+              It&apos;s more than software, it&apos;s your sollution to the
+              world
+            </span>
+          </p>
+          <p>Whatsapp</p>
+          <p>email</p>
+          <p>Location</p>
+        </div>
+        {/* -- */}
+        {/* Text Form Container */}
+        <div className="flex flex-col items-center justify-center border-2 md:w-[500px] md:h-[350px]">
+          {/* name Input */}
+          <div className="flex flex-col w-[350px]">
+            <label className="text-gray-300 text-center"> Name </label>
+            <input
+              type="text"
+              className=" p-2 rounded-lg text-center w-[350px]"
+              placeholder="Enter Your Name"
+            ></input>
+          </div>
+          {/* -- */}
+          {/* Email Input */}
+          <div className="flex flex-col w-[350px]">
+            <label className="text-gray-300 text-center"> Email </label>
+            <input
+              type="text"
+              className=" p-2 rounded-lg text-center"
+              placeholder="Enter your Email."
+            ></input>
+          </div>
+          {/* -- */}
+          {/* Password Input */}
+          <div className="flex flex-col w-[350px] ">
+            <label className="text-gray-300 text-center"> Name </label>
+            <textarea
+              className=" p-2 rounded-lg h-[150px]"
+              placeholder="Enter Message Here."
+            ></textarea>
+          </div>
+          {/* -- */}
+        </div>
+        {/* -- */}
+      </div>
+      {/* Socials Container */}
+      <div className="h-[80px] w-[200px] border-2 mt-3">Socials here</div>
+      {/* -- */}
+    </div>
+  );
 };
 
 export default Contact;
