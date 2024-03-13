bd875de027561c3f46a36b2568aa879a73553ad4
GiftSibiya <giftsibiya.gb@gmail.com>
2024-03-13 07:44:50 +0200

    Project Layout

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,15 +1,80 @@
 "use client";
-import LeftProject from "@/custom modules/LeftProject";
+// Imports
 import React from "react";
+
+// Custom Modules
+import LeftProject from "@/modules/LeftProject";
+import RightProject from "@/modules/RightProject";
 
 const Projects = () => {
   return (
-    <div className="bg-gray-400 ">
-      <LeftProject
-        projectName="Testing Project"
-        projectDescription="This project does many things"
-        projectStack={["HTML", "CSS", "JavaScript", "TypeScript"]}
-      />
+    <div>
+      <div className="flex-col">
+        <h3 className="text-white text-center text-4xl mt-[30px]">Projects</h3>
+
+        {/* Mobile App Projects */}
+        <p className="text-white text-center text-lg mt-[20px]">
+          Mobile Applications
+        </p>
+
+        <LeftProject
+          projectName="Testing Project"
+          projectDescription=" Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.    "
+          projectStack={["HTML", "CSS", "JavaScript", "TypeScript"]}
+        />
+        <RightProject
+          projectName="Testing Project"
+          projectDescription=" Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.    "
+          projectStack={["HTML", "CSS", "JavaScript", "TypeScript"]}
+        />
+        <LeftProject
+          projectName="Testing Project"
+          projectDescription=" Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.    "
+          projectStack={["HTML", "CSS", "JavaScript", "TypeScript"]}
+        />
+        {/* -- */}
+        {/* Mobile App Projects */}
+        <p className="text-white text-center text-lg mt-[20px]">
+          Web Applications
+        </p>
+
+        <RightProject
+          projectName="Testing Project"
+          projectDescription=" Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.    "
+          projectStack={["HTML", "CSS", "JavaScript", "TypeScript"]}
+        />
+        <LeftProject
+          projectName="Testing Project"
+          projectDescription=" Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.    "
+          projectStack={["HTML", "CSS", "JavaScript", "TypeScript"]}
+        />
+        <RightProject
+          projectName="Testing Project"
+          projectDescription=" Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.    "
+          projectStack={["HTML", "CSS", "JavaScript", "TypeScript"]}
+        />
+
+        {/* -- */}
+        {/* Website */}
+        <p className="text-white text-center text-lg mt-[20px]">Websites</p>
+
+        <LeftProject
+          projectName="Testing Project"
+          projectDescription=" Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.    "
+          projectStack={["HTML", "CSS", "JavaScript", "TypeScript"]}
+        />
+        <RightProject
+          projectName="Testing Project"
+          projectDescription=" Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.    "
+          projectStack={["HTML", "CSS", "JavaScript", "TypeScript"]}
+        />
+        <LeftProject
+          projectName="Testing Project"
+          projectDescription=" Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.    "
+          projectStack={["HTML", "CSS", "JavaScript", "TypeScript"]}
+        />
+        {/* -- */}
+      </div>
     </div>
   );
 };
---
+++ src/modules/LeftProject.tsx
@@ -0,0 +1,121 @@
+"use client";
+// Imports
+import React from "react";
+import Image from "next/image";
+
+// Images
+import htmlIcon from "../../public/assets/icons/icons8-html-48.png";
+import cssIcon from "../../public/assets/icons/icons8-css-48.png";
+import JSIcon from "../../public/assets/icons/icons8-javascript-48.png";
+import TSIcon from "../../public/assets/icons/icons8-typescript-48.png";
+import cSharp from "../../public/assets/icons/icons8-c-48.png";
+import reactIcon from "../../public/assets/icons/icons8-react-48.png";
+import nextIcon from "../../public/assets/icons/icons8-nextjs-48.png";
+import sqlIcon from "../../public/assets/icons/icons8-mysql-48.png";
+import mongoIcon from "../../public/assets/icons/MongoDB-48.png";
+
+const iconStyle = ` w-[50px] h-[50px] transition duration-600 ease-in-out hover:scale-150`;
+
+interface LeftProjectProps {
+  projectName: string;
+  projectDescription: string;
+  projectStack: string[];
+}
+
+const LeftProject = ({
+  projectName,
+  projectDescription,
+  projectStack,
+}: LeftProjectProps) => {
+  return (
+    <div className="flex justify-center items-center h-[350px] mt-[20px]">
+      <div className=" flex justify-between w-[90%] ">
+        <div className=" w-[50%] border-2 lg:h-[350px]">Left Side Is Here</div>
+        <div className="w-[400px]">
+          <h4 className="text-white text-2xl text-center">{projectName}</h4>
+          <p className="text-sm text-white my-[20px] text-center">
+            {projectDescription}
+          </p>
+          <div className="flex items-center border-2 h-[100px] justify-between ">
+            {projectStack.map((stack) => {
+              switch (stack) {
+                case "HTML":
+                  return (
+                    <Image
+                      src={htmlIcon}
+                      alt="html"
+                      className={iconStyle}
+                    ></Image>
+                  );
+                case "CSS":
+                  return (
+                    <Image
+                      src={cssIcon}
+                      alt="CSS"
+                      className={iconStyle}
+                    ></Image>
+                  );
+                case "JavaScript":
+                  return (
+                    <Image
+                      src={JSIcon}
+                      alt="Javascript"
+                      className={iconStyle}
+                    ></Image>
+                  );
+                case "TypeScript":
+                  return (
+                    <Image
+                      src={TSIcon}
+                      alt="TypeScript"
+                      className={iconStyle}
+                    ></Image>
+                  );
+                case "C#":
+                  return (
+                    <Image src={cSharp} alt="C#" className={iconStyle}></Image>
+                  );
+                case "React":
+                  return (
+                    <Image
+                      src={reactIcon}
+                      alt="React"
+                      className={iconStyle}
+                    ></Image>
+                  );
+                case "NextJS":
+                  return (
+                    <Image
+                      src={nextIcon}
+                      alt="NextJS"
+                      className={iconStyle}
+                    ></Image>
+                  );
+                case "SQL":
+                  return (
+                    <Image
+                      src={sqlIcon}
+                      alt="SQL"
+                      className={iconStyle}
+                    ></Image>
+                  );
+                case "MongoDB":
+                  return (
+                    <Image
+                      src={mongoIcon}
+                      alt="MongoDB"
+                      className={iconStyle}
+                    ></Image>
+                  );
+                default:
+                  return null;
+              }
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LeftProject;
